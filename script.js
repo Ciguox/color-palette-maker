d17d1a5f0bdcfbd8fe22c553ce8c4743406f8981
David <david.tvz2@gmail.com>
2024-06-09 22:27:25 +0200

    mouse scroll controlls added

--- script.js
+++ script.js
@@ -3,70 +3,81 @@ const samples = document.getElementsByClassName("sample");
 /*generateRandomColor(): generate a single hex code using a random value between 0 and 16777215*/
 function generateRandomColor() {
     let randomColor = Math.floor(Math.random() * 16777215).toString(16).toUpperCase();
-    while(randomColor.length < 6){
+    while (randomColor.length < 6) {
         randomColor = randomColor + "0";
     }
     return randomColor;
 }
-function generateColorScale(colorCode, size = 5){
-    console.log(size)
+function generateColorScale(colorCode, size = 5) {
+    /*Variables*/
     let scale = [];
-    let currColorOnScale;
-    let colorToAdd;
-    colorCode = parseInt(colorCode, 16);
-    while(scale.length < size){
-        if(scale.length == 0){
-            scale.push("FFFFFF");
-        }else if(scale.length < Math.floor(size/2)){
-            scale.push(Math.floor((parseInt(currColorOnScale, 16) + colorCode)/2).toString(16).toUpperCase().substring(0, 6));
-        }else if(scale.length == Math.round(size/2)){
-            scale.push(colorCode.toString(16).toUpperCase().substring(0, 6));
-        }else{
-            colorToAdd = Math.floor((parseInt(currColorOnScale, 16) + parseInt("000000", 16))/2).toString(16).toUpperCase().substring(0, 6)
-            while(colorToAdd.length < 6){
-                colorToAdd = "0" + colorToAdd;
+    let colorToAdd, whiteStep = [], blackStep = [];
+    /*we create 3 lists with the RGB values of color code 000 and fff*/
+    let white = [255, 255, 255], black = [0, 0, 0], colorCodeCopy = colorCode.valueOf();
+    colorCode = colorCode.match(/..?/g);
+    /*executions*/
+    /*Calculate the color step*/
+    for (let i = 0; i < colorCode.length; i++) {
+        whiteStep.push(Math.abs(Math.floor((parseInt(colorCode[i], 16)-white[i])/(size/2))));
+        blackStep.push(Math.abs(Math.floor((parseInt(colorCode[i], 16)-black[i])/(size/2))));
+    }
+    /*Calculate each color on the scale based on the step*/
+    while (scale.length < size) {
+        if (scale.length < Math.floor(size / 2)) {
+            for (let i = 0; i < colorCode.length; i++) {
+                colorCode[i] = white[i] -  whiteStep[i];
+                white[i] = colorCode[i];
+                //DEC to HEX
+                colorCode[i] = colorCode[i].toString(16);
             }
-            scale.push(colorToAdd);
+            colorToAdd = colorCode.join("").toUpperCase().substring(0, 6);
+        } else if (scale.length == Math.floor(size / 2)) {
+            colorCode = colorCodeCopy.match(/..?/g);
+            colorToAdd = colorCodeCopy;
+        } else {
+            for (let i = 0; i < colorCode.length; i++) {
+                colorCode[i] = parseInt(colorCode[i], 16) -  blackStep[i];
+                colorCode[i] = (colorCode[i]<0)?0:colorCode[i];
+                //DEC to HEX
+                colorCode[i] = colorCode[i].toString(16);
+                colorCode[i] = (colorCode[i].length == 1)?"0"+colorCode[i]:colorCode[i];
+            }
+            colorToAdd = colorCode.join("").toUpperCase().substring(0, 6);
         }
-        console.log(scale);
-        currColorOnScale = scale[scale.length - 1];
+        scale.push(colorToAdd);
     }
     return scale;
 }
-function generateTextColor(color){
+function generateTextColor(color) {
     for (let i = 0; i < color.length; i++) {
-        if(i%2 == 0 && color[i] < '5'){
-            console.log(color + " " + color[i])
+        if (i % 2 == 0 && color[i] < '5') {
             return 'FFFFFF';
         }
     }
-    console.log("black");
     return '000000';
 }
 
-function generateColors(numberOfsamples = 5){
+function generateColors(numberOfsamples = 5) {
     let colors = [];
-    //colors = generateColorScale(generateRandomColor());
     /*First we add random colors to the pallette*/
-
     for (let i = 0; i < numberOfsamples; i++) {
         colors.push(generateRandomColor());
     }
-
     /*We order them from low to hight or reverse at random*/
-    (Math.random < 0.5)?colors.sort():colors.sort().reverse();
+    (Math.random < 0.5) ? colors.sort() : colors.sort().reverse();
     /*The resulting pallete is shown in the web*/
     for (let i = 0; i < numberOfsamples; i++) {
-        const sample = document.getElementById(`color-${i+1}`);
+        const sample = document.getElementById(`color-${i + 1}`);
         sample.style = `background-color:#${colors[i]}; color: #${generateTextColor(colors[i])};`;
         sample.textContent = colors[i];
     }
-}    
-function addColorScale(scale){
+}
+function addColorScale(scale) {
     let hueSamples = document.createDocumentFragment();
     for (let i = 0; i < scale.length; i++) {
         const sample = document.createElement("div");
-        sample.style = `background-color:#${scale[i]}; color: #${generateTextColor(scale[i])};`;
+        console.log(100 / scale.length);
+        sample.style = `background-color:#${scale[i]}; color: #${generateTextColor(scale[i])}; height: ${Math.floor(100 / scale.length)}vh;`;
         sample.textContent = scale[i];
         sample.classList.add("hue-sample");
         hueSamples.appendChild(sample);
@@ -82,9 +93,19 @@ document.addEventListener("keypress", (event) => {
 });
 
 for (let i = 0; i < samples.length; i++) {
-    samples[i].addEventListener("mouseover", () =>{
-        if(samples[i].childNodes.length < 30){
-            samples[i].append(addColorScale(generateColorScale(samples[i].textContent, 30)));
-        }        
-    });
+    samples[i].addEventListener("mouseenter", () => {
+        console.log("Event added");
+        samples[i].addEventListener("wheel", () =>{
+            if (samples[i].childNodes.length < 10) {
+                const temp = samples[i].textContent;
+                samples[i].textContent = "";
+                samples[i].append(addColorScale(generateColorScale(temp, 10)));
+            }
+        }, {passive: true});
+        samples[i].addEventListener("mouseleave", () => {
+            if (samples[i].childNodes.length > 1) {
+                samples[i].innerHTML = samples[i].childNodes[Math.floor(samples[i].childNodes.length/2)].textContent;
+            }
+        })
+    }, {once : true});
 }
